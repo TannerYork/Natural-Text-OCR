000ca286eeca7c69f2e5b3e5bc3048905e377bcd
Tanner York <tannerwyork@gmail.com>
2020-06-01 18:16:34 -0500

    Add tesseract/EAST text recognition and layout for transcribe text function

---
+++ transcribe_text.py
@@ -0,0 +1,8 @@
+#!python
+
+from models.tesseract_east.text_recognition import text_recognition
+
+
+def transcribe_text():
+    """Takes in an image and returns the text detected in it"""
+    pass
